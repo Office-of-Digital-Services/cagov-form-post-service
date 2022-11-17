83a7211bd3076cbfbc01ede9b91ed31ce107ea32
Carter Medlin <carterm@users.noreply.github.com>
2022-11-16 17:13:24 -0800

    Azure function runtime 4 configured.

--- air-table/index.js
+++ air-table/index.js
@@ -2,19 +2,17 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.method === 'GET') {
-        //Hitting the service by default will show the index page.
         context.res = {
-            body: fs.readFileSync(`${appName}/index.html`, 'utf8'),
+            body: GetResponseHTML,
             headers: {
                 'Content-Type': 'text/html'
             }
         };
 
-        context.done();
         return;
     }
 
-    const name = (req.query.name || (req.body && req.body.name));
+    const name = req.query.name || req.body;
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
@@ -24,3 +22,26 @@ module.exports = async function (context, req) {
         body: responseMessage
     };
 }
+
+const GetResponseHTML = `
+<html>
+    <head>
+        <title>
+            cagov-form-post-service AirTable
+        </title>
+    </head>
+    <body>
+        <h1>cagov-form-post-service AirTable</h1>
+
+    <p>What do you want to do?</p>
+    <ul>
+      <li>
+        <form method="post">
+          <input type="text" name="name" value="TEST" />
+          <input type="submit" value="Manual post submission"/>
+        </form>
+      </li>
+    </ul>
+    </body>
+</html>
+`;
