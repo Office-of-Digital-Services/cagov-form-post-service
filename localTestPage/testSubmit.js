1f39e37a4c9d759518dea5b63ac3aa1c15c99dfd
Carter Medlin <carterm@users.noreply.github.com>
2022-11-21 15:36:26 -0800

    Code simplify

--- localTestPage/testSubmit.js
+++ localTestPage/testSubmit.js
@@ -36,23 +36,9 @@ async function submitForm(/** @type { SubmitEvent } */ e) {
     "Content-Type": "application/json",
   };
 
-  const response = await performPostHttpRequest(localTarget, headers, jsonFormData);
+  const response = await fetch(localTarget, { method: "POST", headers: headers, body: JSON.stringify(jsonFormData) })
+    .then(r => r.json());
+
   console.log(response);
   alert(JSON.stringify(response, null, 2));
-}
-
-async function performPostHttpRequest(/** @type {String} */ fetchLink,/** @type {HeadersInit} */ headers, /** @type {any} */ body) {
-  try {
-    const rawResponse = await fetch(fetchLink, {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(body)
-    });
-    const content = await rawResponse.json();
-    return content;
-  }
-  catch (err) {
-    console.error(`Error at fetch POST: ${err}`);
-    throw err;
-  }
 }
