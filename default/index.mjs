a6fd20f29f73a16d75ea5e48a445aec686720818
Carter Medlin <carterm@users.noreply.github.com>
2023-05-11 11:13:02 -0700

    adding root support with robots.txt and 404s

---
+++ default/index.mjs
@@ -0,0 +1,27 @@
+//@ts-check
+import fs from "fs";
+/**
+ * @type {import("@azure/functions").AzureFunction}
+ * @param {import("@azure/functions").HttpRequest} req
+ */
+export default async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+
+  const res = /** @type { import("@azure/functions").HttpResponseFull} */ (
+    context.res
+  );
+
+  switch (req.params.page?.trim().toLowerCase()) {
+    case "robots.txt":
+      res.body = fs.readFileSync(
+        `${context.executionContext.functionDirectory}/robots.txt`,
+        "utf8"
+      );
+
+      res.set("Cache-Control", "max-age=2592000"); //1 month
+      break;
+    default:
+      res.body = "File not found.";
+      res.status(404);
+  }
+}
