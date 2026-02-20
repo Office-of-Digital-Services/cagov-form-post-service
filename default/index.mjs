63fd5d0dc932f678a6fd1141279d00bacb19ab46
Carter Medlin <carterm@users.noreply.github.com>
2026-02-19 16:16:47 -0800

    default working again

--- default/index.mjs
+++ default/index.mjs
@@ -1,39 +1,42 @@
 //@ts-check
 import fs from "fs";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /**
- * @type {import("@azure/functions").AzureFunction}
- * @param {import("@azure/functions").Context} context
+ * @param {import("@azure/functions").InvocationContext} context
  * @param {import("@azure/functions").HttpRequest} req
  */
 export default async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
-  const res = /** @type { import("@azure/functions").HttpResponseFull} */ (
-    context.res
-  );
+  const res = {
+    /** @type {Record<string, string>} */
+    headers: {},
+    status: 200,
+    body: ""
+  };
 
   const file = req.params.page?.trim().toLowerCase() || "index";
 
   switch (file) {
     case "robots.txt":
-      res.body = fs.readFileSync(
-        `${context.executionContext.functionDirectory}/robots.txt`,
-        "utf8"
-      );
+      res.body = fs.readFileSync(`${__dirname}/robots.txt`, "utf8");
+      res.headers["Cache-Control"] = "max-age=2592000"; //1 month
 
-      res.set("Cache-Control", "max-age=2592000"); //1 month
       break;
     case "index":
     case "sample":
     case "success":
-      res.body = fs.readFileSync(
-        `${context.executionContext.functionDirectory}/${file}.html`,
-        "utf8"
-      );
-      res.type("html");
+      res.body = fs.readFileSync(`${__dirname}/${file}.html`, "utf8");
+
+      res.headers["Content-Type"] = "text/html";
       break;
     default:
       res.body = `File not found - ${file}`;
-      res.status(404);
+      res.status = 404;
   }
+  return res;
 }
---
+++ index.mjs
@@ -0,0 +1,34 @@
+import { app } from "@azure/functions";
+import v2 from "./v2/index.mjs";
+import mydefault from "./default/index.mjs";
+
+app.http("helloPost", {
+  route: "hello",
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    const response = await v2(context, request);
+
+    const body = await request.json();
+    return { jsonBody: { message: `Hello ${body.name || "world"}` } };
+  }
+});
+
+app.http("helloGet", {
+  route: "hello",
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log("GET /hello");
+    return { jsonBody: { message: "Hello from GET" } };
+  }
+});
+
+app.http("default", {
+  route: "{page?}",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  authLevel: "anonymous",
+  handler: async (request, context, test, foo) => {
+    return await mydefault(context, request);
+  }
+});
