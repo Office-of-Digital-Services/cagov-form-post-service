3640df5aadbca471d0263b269bda7f59ed4811ba
Carter Medlin <carterm@users.noreply.github.com>
2026-03-23 13:23:08 -0700

    protects against bad new configs breaking the whole thing

--- functions/v2/support/serverList.mjs
+++ functions/v2/support/serverList.mjs
@@ -3,49 +3,44 @@
 // AitTable Base and Table IDs are not treated as secrets
 /**
  * @typedef {object} ServerConfig
- * @property {string} host
- * @property {string} name
  * @property {string} airtableBaseId
  * @property {string} airtableTable
  * @property {string} airtableToken
  * @property {string} recaptchaSecret
  */
 
-const getEnvVar = (/** @type {string} */ key) => {
+const getEnvVar = (
+  /** @type {string} */ key,
+  /** @type {boolean?} */ required = true
+) => {
   const value = process.env[key];
-  if (!value) {
+  if (required && !value) {
     throw new Error(`Missing environment variable: ${key}`);
   }
-  return value;
+  return value || "";
 };
 
-const hostListString = getEnvVar("HostList");
-/** @type {Array<ServerConfig>} */
-const jsonServerList = hostListString.split(",").map(name => {
-  const host = getEnvVar(`${name}_host`);
-  const airtableTable = getEnvVar(`${name}_airtableTable`);
-  const airtableBaseId = getEnvVar(`${name}_airtableBaseId`);
-  const airtableToken = getEnvVar(`${name}_airtableToken`);
-  const recaptchaSecret = getEnvVar(`${name}_recaptchaSecret`);
-
-  return {
-    name,
-    host,
-    airtableBaseId,
-    airtableTable,
-    airtableToken,
-    recaptchaSecret
-  };
-});
-
 /**
- * @param {string} host
+ * @param {string} host Host URL from request header
  */
 function getServerConfigByHost(host) {
-  const serverConfig = jsonServerList.find(server => server.host === host);
-  if (!serverConfig) {
+  const hostList = getEnvVar("HostList").split(",");
+  const name = hostList.find(onename => {
+    const serverHost = getEnvVar(`${onename}_host`, false);
+    return serverHost === host;
+  });
+  if (!name) {
     throw new Error(`No server configuration found for host: ${host}`);
   }
+
+  /** @type {ServerConfig} */
+  const serverConfig = {
+    airtableBaseId: getEnvVar(`${name}_airtableBaseId`),
+    airtableTable: getEnvVar(`${name}_airtableTable`),
+    airtableToken: getEnvVar(`${name}_airtableToken`),
+    recaptchaSecret: getEnvVar(`${name}_recaptchaSecret`)
+  };
+
   return serverConfig;
 }
 
