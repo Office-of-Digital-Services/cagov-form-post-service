06e850cb4eacca9ecaee41254ab41c565f8d9d16
Carter Medlin <carterm@users.noreply.github.com>
2026-03-24 14:24:34 -0700

    trying OPTIONS handler

--- index.mjs
+++ index.mjs
@@ -13,3 +13,19 @@ app.http("default", {
   methods: ["GET"],
   handler: mydefault
 });
+
+app.http("v2Options", {
+  methods: ["OPTIONS"],
+  route: "v2/{*path}",
+  handler: async () => {
+    return {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type,Authorization",
+        "Access-Control-Max-Age": "86400"
+      }
+    };
+  }
+});
