5f85951e50867f80f05996f8ca9bfe44faa18cf3
Carter Medlin <carterm@users.noreply.github.com>
2026-03-24 15:43:22 -0700

    no dynamic CORS

--- index.mjs
+++ index.mjs
@@ -13,19 +13,3 @@ app.http("default", {
   methods: ["GET"],
   handler: mydefault
 });
-
-app.http("v2Options", {
-  route: "api/v2/{*path}",
-  methods: ["OPTIONS"],
-  handler: async () => {
-    return {
-      status: 204,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type,Authorization",
-        "Access-Control-Max-Age": "86400"
-      }
-    };
-  }
-});
