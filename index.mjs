1c43f491aa89c1ae7fe5bf8a13f567a01eac866d
Carter Medlin <carterm@users.noreply.github.com>
2026-03-30 09:27:41 -0700

    New API path

--- index.mjs
+++ index.mjs
@@ -2,12 +2,14 @@ import { app } from "@azure/functions";
 import v2 from "./functions/v2/index.mjs";
 import mydefault from "./functions/default/index.mjs";
 
+// API route for version 2 of the API
 app.http("v2", {
-  route: "api/v2",
-  methods: ["GET", "POST"],
+  route: "api/v2/{*path}",
+  methods: ["GET", "POST", "OPTIONS"],
   handler: v2
 });
 
+// Default route which serves static files from the "public" directory
 app.http("default", {
   route: "{page?}",
   methods: ["GET"],
